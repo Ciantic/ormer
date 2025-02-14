60c2bdfe333869b6362d5995e2f11b606e4f5645
Jari Pennanen <ciantic@oksidi.com>
2025-02-14 15:06:43 +0200

    nullable?

--- src/sqlite.ts
+++ src/sqlite.ts
@@ -10,6 +10,7 @@ import {
     rowVersion,
     table,
     updatedAt,
+    nullable,
 } from "./lib.ts";
 
 export function sqliteCreateTables<
@@ -29,7 +30,7 @@ function buildSqliteCreateTable<T extends Record<string, ColumnType<ColumnKind>>
 }
 
 function buildSqliteColumn([name, column]: [string, ColumnType<ColumnKind>]) {
-    return `${name} ${buildSqliteColumnTypeStr(column)}`;
+    return `"${name}" ${buildSqliteColumnTypeStr(column, name)}`;
 }
 
 const COLUMN_TYPE_MAP = {
@@ -42,14 +43,22 @@ const COLUMN_TYPE_MAP = {
     Array: "TEXT", // JSON ARRAY
 };
 
-function buildSqliteColumnTypeStr(column: ColumnType<ColumnKind>) {
+function buildSqliteColumnTypeStr(column: ColumnType<ColumnKind>, name: string) {
     let schema = column.select;
     let type = schema.expects as keyof typeof COLUMN_TYPE_MAP;
     let isOptional = false;
+    let isNullable = false;
+    let constraints = "";
     if (schema.type === "optional") {
         const optschema = schema as v.OptionalSchema<v.AnySchema, undefined>;
         type = optschema.wrapped.expects as keyof typeof COLUMN_TYPE_MAP;
         isOptional = true;
+    }
+
+    if (schema.type === "nullable") {
+        const nullschema = schema as v.NullableSchema<v.AnySchema, undefined>;
+        type = nullschema.wrapped.expects as keyof typeof COLUMN_TYPE_MAP;
+        isNullable = true;
     }
 
     if ("pipe" in schema) {
@@ -67,11 +76,21 @@ function buildSqliteColumnTypeStr(column: ColumnType<ColumnKind>) {
         throw new Error(`Error sqliteColumnType is not known: ${type}`);
     }
 
-    const nullnessStr = isOptional ? "" : "NOT NULL";
+    if (type === "Object") {
+        constraints = `CHECK (json_valid("${name}"))`;
+    }
+
+    if (type === "Array") {
+        constraints = `CHECK (json_valid("${name}") AND json_type("${name}") = 'array')`;
+    }
+
+    const nullnessStr = isOptional || isNullable ? "" : "NOT NULL";
     const primaryKeyStr = column.kind === "primaryKey" ? "PRIMARY KEY" : "";
     const autoIncStr = column.autoIncrement ? "AUTOINCREMENT" : "";
 
-    return [columnTypeStr, nullnessStr, primaryKeyStr, autoIncStr].filter((f) => f).join(" ");
+    return [columnTypeStr, nullnessStr, primaryKeyStr, autoIncStr, constraints]
+        .filter((f) => f)
+        .join(" ");
 }
 
 Deno.test("sqliteCreateTables", () => {
@@ -82,6 +101,7 @@ Deno.test("sqliteCreateTables", () => {
         some_date: col(v.date()),
         some_bigint: col(v.bigint()),
         some_number: col(v.number()),
+        some_nullable_number: nullable(col(v.number())),
         some_rowversion: rowVersion(),
         some_json_col: col(
             v.object({
@@ -97,17 +117,18 @@ Deno.test("sqliteCreateTables", () => {
     assertEquals(
         sqliteCreateTables(exampleTable),
         `CREATE TABLE example (
-            some_autoinc_primarykey INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            some_string TEXT NOT NULL,
-            some_optstring TEXT,
-            some_date TEXT NOT NULL,
-            some_bigint TEXT NOT NULL,
-            some_number REAL NOT NULL,
-            some_rowversion INTEGER NOT NULL,
-            some_json_col TEXT NOT NULL,
-            some_json_array_col TEXT NOT NULL,
-            some_created_at TEXT NOT NULL,
-            some_updated_at TEXT NOT NULL
+            "some_autoinc_primarykey" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            "some_string" TEXT NOT NULL,
+            "some_optstring" TEXT,
+            "some_date" TEXT NOT NULL,
+            "some_bigint" TEXT NOT NULL,
+            "some_number" REAL NOT NULL,
+            "some_nullable_number" REAL,
+            "some_rowversion" INTEGER NOT NULL,
+            "some_json_col" TEXT NOT NULL CHECK (json_valid("some_json_col")),
+            "some_json_array_col" TEXT NOT NULL CHECK (json_valid("some_json_array_col") AND json_type("some_json_array_col") = 'array'),
+            "some_created_at" TEXT NOT NULL,
+            "some_updated_at" TEXT NOT NULL
         );`
             .replaceAll("        ", "")
             .trim()
