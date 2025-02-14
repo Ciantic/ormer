8bf4d6070d28b2af488c1ad5215a6a04ec34b578
Jari Pennanen <ciantic@oksidi.com>
2025-02-14 19:44:43 +0200

    Move to test file

--- src/sqlite.ts
+++ src/sqlite.ts
@@ -1,7 +1,5 @@
-import * as v from "npm:valibot";
-import { assertEquals, assertThrows } from "jsr:@std/assert";
+import type * as v from "npm:valibot";
 import type { Table, ColumnKind, ColumnType } from "./lib.ts";
-import * as o from "./lib.ts";
 
 export function sqliteCreateTables<
     T extends readonly Table<any, Record<string, ColumnType<ColumnKind>>>[]
@@ -107,72 +105,3 @@ function buildSqliteColumnTypeStrSchema(
 function buildSqliteColumnTypeStr(column: ColumnType<ColumnKind>, name: string) {
     return buildSqliteColumnTypeStrSchema(column, name, column.select, {});
 }
-
-Deno.test("optional throws", () => {
-    const exampleTable = o.table("example", {
-        some_optional_string: o.col(v.optional(v.string())),
-    });
-    assertThrows(
-        () => sqliteCreateTables(exampleTable),
-        Error,
-        "Optional should be handled by nullable"
-    );
-});
-
-Deno.test("sqliteCreateTables", () => {
-    const exampleTable = o.table("example", {
-        // Key types
-        some_autoinc_primarykey: o.pkAutoInc(),
-        some_rowversion: o.rowVersion(),
-
-        // String types
-        some_string: o.string(),
-        some_varchar: o.varchar(255),
-
-        // Date types
-        some_date: o.col(v.date()),
-
-        // Numeric types
-        some_integer: o.integer(),
-        some_bigint: o.bigint(),
-        some_number: o.float(),
-
-        // Nullability
-        some_nullable_string: o.nullable(o.string()),
-        some_nullable_integer: o.nullable(o.integer()),
-
-        // JSON types
-        some_json_col: o.json({
-            foo: v.string(),
-            bar: v.number(),
-        }),
-        some_json_array_col: o.array(v.string()),
-
-        // Auto populated timestamps
-        some_created_at: o.createdAt(),
-        some_updated_at: o.updatedAt(),
-    });
-
-    const value = sqliteCreateTables(exampleTable);
-    assertEquals(
-        value,
-        `CREATE TABLE example (
-            "some_autoinc_primarykey" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            "some_rowversion" INTEGER NOT NULL,
-            "some_string" TEXT NOT NULL,
-            "some_varchar" TEXT NOT NULL CHECK (length("some_varchar") <= 255),
-            "some_date" TEXT NOT NULL,
-            "some_integer" INTEGER NOT NULL,
-            "some_bigint" TEXT NOT NULL,
-            "some_number" REAL NOT NULL,
-            "some_nullable_string" TEXT,
-            "some_nullable_integer" INTEGER,
-            "some_json_col" TEXT NOT NULL CHECK (json_valid("some_json_col")),
-            "some_json_array_col" TEXT NOT NULL CHECK (json_valid("some_json_array_col") AND json_type("some_json_array_col") = 'array'),
-            "some_created_at" TEXT NOT NULL,
-            "some_updated_at" TEXT NOT NULL
-        );`
-            .replaceAll("        ", "")
-            .trim()
-    );
-});
