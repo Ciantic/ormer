f2811b0e894fa4cfcc9066a232a3cbf8f0105041
Jari O. O. Pennanen <ciantic@oksidi.com>
2026-04-19 01:33:58 +0300

    More fk

--- examples/zod-inference.ts
+++ examples/zod-inference.ts
@@ -1,76 +1,77 @@
 import { z } from "zod";
 
 
-type Ext<S> = { _ext: S };
-type Ext2<S, T> = { _ext: S, _ext2: T };
+type DbType<S> = { _dbtype: S };
+type Fk<R extends z.ZodObject<any>, C extends keyof R["shape"]> = { _dbtype: "foreignKey", _table: R, _column: C };
 
 
 // Type augmentation
 declare module "zod" {
     interface ZodType {
-        int64<T extends z.ZodInt>(this: T): T & Ext<"int64">;
-        int32<T extends z.ZodInt32>(this: T): T & Ext<"int32">;
-        float32<T extends z.ZodNumber>(this: T): T & Ext<"float32">;
-        float64<T extends z.ZodNumber>(this: T): T & Ext<"float64">;
-        pk<T extends z.ZodUUID>(this: T): T & Ext<"pk">;
-        pk<T extends z.ZodString>(this: T): T & Ext<"pk">;
-        pkAutoInc<T extends z.ZodInt>(this: T): T & Ext<"pkAutoInc">;
-        foreignKey<T extends z.ZodUUID, R extends z.ZodObject<any>>(this: T, table: R, column: keyof R["shape"]): T & Ext2<"foreignKey", { table: R, column: keyof R["shape"] }>;
-        foreignKey<T extends z.ZodString, R extends z.ZodObject<any>>(this: T, table: R, column: keyof R["shape"]): T & Ext2<"foreignKey", { table: R, column: keyof R["shape"] }>;
-        foreignKey<T extends z.ZodInt, R extends z.ZodObject<any>>(this: T, table: R, column: keyof R["shape"]): T & Ext2<"foreignKey", { table: R, column: keyof R["shape"] }>;
-        rowversion<T extends z.ZodInt>(this: T): T & Ext<"rowversion">;
-        concurrencyStamp<T extends z.ZodString>(this: T): T & Ext<"concurrencyStamp">;
-        timestamptz<T extends z.ZodDate>(this: T): T & Ext<"timestamptz">;
-        timestamp<T extends z.ZodDate>(this: T): T & Ext<"timestamp">;
-        dateOnly<T extends z.ZodISODate>(this: T): T & Ext<"dateOnly">;
-        timeOnly<T extends z.ZodISOTime>(this: T): T & Ext<"timeOnly">;
+        int64<T extends z.ZodInt>(this: T): T & DbType<"int64">;
+        int32<T extends z.ZodInt32>(this: T): T & DbType<"int32">;
+        float32<T extends z.ZodNumber>(this: T): T & DbType<"float32">;
+        float64<T extends z.ZodNumber>(this: T): T & DbType<"float64">;
+        pk<T extends z.ZodUUID>(this: T): T & DbType<"pk">;
+        pk<T extends z.ZodString>(this: T): T & DbType<"pk">;
+        pkAutoInc<T extends z.ZodInt>(this: T): T & DbType<"pkAutoInc">;
+        foreignKey<T extends z.ZodUUID, R extends z.ZodObject<any>, C extends keyof R["shape"]>(this: T, table: R, column: C): T & Fk<R, C>;
+        foreignKey<T extends z.ZodString, R extends z.ZodObject<any>, C extends keyof R["shape"]>(this: T, table: R, column: C): T & Fk<R, C>;
+        foreignKey<T extends z.ZodInt, R extends z.ZodObject<any>, C extends keyof R["shape"]>(this: T, table: R, column: C): T & Fk<R, C>;
+        rowversion<T extends z.ZodInt>(this: T): T & DbType<"rowversion">;
+        concurrencyStamp<T extends z.ZodString>(this: T): T & DbType<"concurrencyStamp">;
+        timestamptz<T extends z.ZodDate>(this: T): T & DbType<"timestamptz">;
+        timestamp<T extends z.ZodDate>(this: T): T & DbType<"timestamp">;
+        dateOnly<T extends z.ZodISODate>(this: T): T & DbType<"dateOnly">;
+        timeOnly<T extends z.ZodISOTime>(this: T): T & DbType<"timeOnly">;
 
         /**
          * Reference field (isn't part of database schema)
          * @param this 
          */
-        ref<T extends z.ZodObject, S extends string>(this: T): T & Ext<"ref">;
+        ref<T extends z.ZodObject>(this: T): T & DbType<"ref">;
     }
 }
-z.ZodType.prototype.int64 = function(this: z.ZodType & Ext<any>) {
-    this._ext = "int64";
+z.ZodType.prototype.int64 = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "int64";
     return this;
 }
-z.ZodType.prototype.int32 = function(this: z.ZodType & Ext<any>) {
-    this._ext = "int32";
+z.ZodType.prototype.int32 = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "int32";
     return this;
 }
-z.ZodType.prototype.float32 = function(this: z.ZodType & Ext<any>) {
-    this._ext = "float32";
+z.ZodType.prototype.float32 = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "float32";
     return this;
 }
-z.ZodType.prototype.float64 = function(this: z.ZodType & Ext<any>) {
-    this._ext = "float64";
+z.ZodType.prototype.float64 = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "float64";
     return this;
 }
-z.ZodType.prototype.pk = function(this: z.ZodType & Ext<any>) {
-    this._ext = "pk";
+z.ZodType.prototype.pk = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "pk";
     return this;
 }
-z.ZodType.prototype.pkAutoInc = function(this: z.ZodType & Ext<any>) {
-    this._ext = "pkAutoInc";
+z.ZodType.prototype.pkAutoInc = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "pkAutoInc";
     return this;
 }
-z.ZodType.prototype.rowversion = function(this: z.ZodType & Ext<any>) {
-    this._ext = "rowversion";
+z.ZodType.prototype.rowversion = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "rowversion";
     return this;
 }
-z.ZodType.prototype.concurrencyStamp = function(this: z.ZodType & Ext<any>) {
-    this._ext = "concurrencyStamp";
+z.ZodType.prototype.concurrencyStamp = function(this: z.ZodType & DbType<any>) {
+    this._dbtype = "concurrencyStamp";
     return this;
 }
-z.ZodType.prototype.foreignKey = function<T extends z.ZodType, R extends z.ZodObject<any>>(this: T & Ext2<T, R>, table: R, column: keyof R["shape"]) {
-    this._ext = "foreignKey" as any;
-    this._ext2 = { table, column } as any;
+z.ZodType.prototype.foreignKey = function<T extends z.ZodType, R extends z.ZodObject<any>, C extends keyof R["shape"]>(this: T & Fk<R, C>, table: R, column: C) {
+    this._dbtype = "foreignKey";
+    this._table = table;
+    this._column = column;
     return this as any;
 }
-z.ZodType.prototype.ref = function<T extends z.ZodObject, S extends string>(this: T & Ext<any>) {
-    this._ext = "ref";
+z.ZodType.prototype.ref = function<T extends z.ZodObject>(this: T & DbType<any>) {
+    this._dbtype = "ref";
     return this;
 }
 
